36d04fbe7ba1379f25c5f3e1d4a5e111b9ec7143
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-02-14 14:02:36 +0800

    Fix pre-commit hook

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-require('dotenv').config();
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
 
 const {
   SERVER_PORT,
   MONGO_URL,
   MONGO_PORT,
-  MONGO_DB_NAME,
-} = process.env;
+  MONGO_DB_NAME
+} = process.env
 
-const app = express();
+const app = express()
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/${MONGO_DB_NAME}`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  useUnifiedTopology: true
+})
 mongoose.connection
   .once('open', () => console.log('Connected to mongodb database'))
-  .on('error', (error) => console.log(`Error: ${error}`));
+  .on('error', (error) => console.log(`Error: ${error}`))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World')
+})
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server is running on port ${SERVER_PORT}`);
-});
+  console.log(`Server is running on port ${SERVER_PORT}`)
+})
