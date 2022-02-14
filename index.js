017fc5ec52de0757aae841297bcdf67e3fadb1f9
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-02-14 13:00:09 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const {
+  SERVER_PORT,
+  MONGO_URL,
+  MONGO_PORT,
+  MONGO_DB_NAME,
+} = process.env;
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/${MONGO_DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection
+  .once('open', () => console.log('Connected to mongodb database'))
+  .on('error', (error) => console.log(`Error: ${error}`));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
+});
